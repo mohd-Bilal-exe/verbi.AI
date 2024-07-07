fe82b70b95bb8993b7770441b99418449d425bed
Mohd Bilal <bilalmail7054@gmail.com>
2024-07-07 13:54:34 +0530

    Profile page banane ka mann tohnhii kr rha prr banane k baad dopamine milega

---
+++ src/components/EditModal.jsx
@@ -0,0 +1,15 @@
+export default function EditModal({ attributes, onClose }) {
+  return (
+    <section className="absolute z-50 top-0 left-0 w-screen h-screen backdrop-blur-lg flex justify-center items-center">
+      <div className="bg-black p-4 rounded-lg shadow-lg">
+        <h2>{attributes}</h2>
+        <button
+          onClick={onClose}
+          className="mt-4 p-2 bg-red-500 text-white rounded"
+        >
+          Close
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import { m } from "framer-motion";
 import { useSelector, useDispatch } from "react-redux";
 import { darkMode, logoutUser } from "../Redux/Actions";
 import { useNavigate } from "react-router-dom";
 import { Moon, Sun } from "@phosphor-icons/react";
 import { EditIcon } from "../components/SvgIcons";
+import EditModal from "../components/EditModal";
 
 export default function ProfilePage() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.userDetails);
   const isDarkMode = useSelector((state) => state.darkMode);
   const navigate = useNavigate();
+  const [editSection, setEditSection] = useState(null);
 
-  const handleEdit = () => {
-    navigate("edit");
+  const handleEdit = (section) => {
+    setEditSection(section);
   };
 
   const toggleDarkMode = () => {
@@ -24,10 +27,14 @@ export default function ProfilePage() {
     navigate("/login");
   };
 
+  const closeModal = () => {
+    setEditSection(null);
+  };
+
   const profileSection = (
     <section
       key="profile"
-      className={`flex justify-between my-8  p-4 rounded-xl bg-gradient-to-tl ${
+      className={`flex justify-between my-8 p-4 rounded-xl bg-gradient-to-tl ${
         isDarkMode
           ? "from-foreground to-Lightcopy-lighter/30"
           : "from-foreground/10 to-foreground/20"
@@ -50,13 +57,13 @@ export default function ProfilePage() {
           </h2>
         </div>
       </div>
-      <div className={`flex flex-col justify-between items-start`}>
+      <div className="flex flex-col justify-between items-start">
         <button
           onClick={toggleDarkMode}
-          className={`overflow-hidden  p-1.5 rounded-full w-10 h-10 border ${
+          className={`overflow-hidden p-1.5 rounded-full w-10 h-10 border ${
             isDarkMode
               ? "bg-copy-lighter/10 hover:bg-copy/20 border-copy-lighter hover:border-copy"
-              : " bg-copy-light hover:bg-copy-lighter/50 border-copyLight hover:border-black"
+              : "bg-copy-light hover:bg-copy-lighter/50 border-copyLight hover:border-black"
           } transition-all`}
         >
           <span
@@ -64,13 +71,13 @@ export default function ProfilePage() {
               isDarkMode ? "" : "-translate-y-9"
             } transition-all ease-in-out`}
           >
-            <Moon size={"100%"} className={`pb-0.5`} weight="duotone" />
-            <Sun size={"100%"} weight="duotone" />{" "}
+            <Moon size={"100%"} className="pb-0.5" weight="duotone" />
+            <Sun size={"100%"} weight="duotone" />
           </span>
         </button>
         <button
-          onClick={handleEdit}
-          className={` flex justify-center items-center  p-1.5 rounded-full w-10 h-10 hover:text-blue-500  transition-all`}
+          onClick={() => handleEdit("Name")}
+          className="flex justify-center items-center p-1.5 rounded-full w-10 h-10 hover:text-blue-500 transition-all"
         >
           <EditIcon />
         </button>
@@ -83,7 +90,7 @@ export default function ProfilePage() {
       <div className="flex flex-col">
         <span className="text-lg font-semibold">Nickname: {user.nickname}</span>
         <p className="text-base"></p>
-        <span className="text-lg font-semibold">About:{user.about}</span>
+        <span className="text-lg font-semibold">About: {user.about}</span>
         <p className="text-base"></p>
       </div>
     </section>
@@ -92,7 +99,7 @@ export default function ProfilePage() {
   const preferencesSection = (
     <section key="preferences" className="my-8">
       <div className="flex flex-col">
-        <span className="text-lg font-semibold">Tone:{user.tone}</span>
+        <span className="text-lg font-semibold">Tone: {user.tone}</span>
         <span className="text-lg font-semibold">Nature: {user.nature}</span>
       </div>
     </section>
@@ -100,25 +107,28 @@ export default function ProfilePage() {
 
   return (
     <m.section
-      key={"ProfilePage"}
+      key="ProfilePage"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, type: "spring" }}
       className={`w-screen h-screen px-5 pt-4 ${
-        isDarkMode ? " text-copy" : "text-copyLight"
+        isDarkMode ? "text-copy" : "text-copyLight"
       }`}
     >
-      <div className={`w-full h-full`}>
+      <div className="w-full h-full">
         {profileSection}
         {otherInfoSection}
         {preferencesSection}
         <button
           onClick={handleLogout}
-          className={`mt-9 p-2 border-2 border-red-500 rounded-lg text-red-500 text-sm hover:bg-red-500/20 `}
+          className="mt-9 p-2 border-2 border-red-500 rounded-lg text-red-500 text-sm hover:bg-red-500/20"
         >
           Logout
         </button>
+        {editSection && (
+          <EditModal attributes={editSection} onClose={closeModal} />
+        )}
       </div>
     </m.section>
   );
