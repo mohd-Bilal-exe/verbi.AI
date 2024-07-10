dc2f5237bf4eef9f4edf90deb69aa3a9672bb0b3
Mohd Bilal <bilalmail7054@gmail.com>
2024-07-10 23:49:31 +0530

    Several chnages

--- src/pages/grammarPage.jsx
+++ src/pages/grammarPage.jsx
@@ -19,34 +19,13 @@ const GrammarPage = () => {
   const handleGrammarCheck = async () => {
     setLoading(true);
     try {
-      //const response = await grammarCheck(ipText, customInstructions);
-      const response = await fetch(
-        "https://verbi-ai.onrender.com/AI/grammarCheck",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: ipText,
-            instructions: customInstructions,
-          }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-
-      const responseData = await response.json();
-      setOpText(responseData.text);
-
+      const response = await grammarCheck(ipText, customInstructions);
+      setOpText(response);
       const grammarObj = {
         ipText: ipText,
         time: formatTimefunc(Date.now()),
-        opText: responseData.text,
+        opText: response,
       };
-
       dispatch(
         globalHistory({
           type: "Grammar Check",
